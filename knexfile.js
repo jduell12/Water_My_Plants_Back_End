9f3627a8f71a3656aea0d0aa0236719c22b6d0eb
Jessica <duellje@oregonstate.edu>
2020-10-27 18:57:36 -0400

    added knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,51 @@
-// Update with your config settings.
+const pgConnection =
+  process.env.DATABASE_URL || "postgresql://postgres@localhost/water_my_plants";
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+    client: "pg",
+    connection: "postgresql://postgres@localhost/water_my_plants_test",
+    migrations: {
+      directory: "./db/migrations",
+    },
+    seeds: {
+      directory: "./db/seeds",
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
+    useNullAsDefault: true,
+  },
+
+  testing: {
+    client: "pg",
+    connection: "postgresql://postgres@localhost/water_my_plants_test",
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: "./db/migrations",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    seeds: {
+      directory: "./db/seeds",
+    },
+    useNullAsDefault: true,
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: "postgresql",
+    connection: pgConnection,
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      directory: "./db/migrations",
+    },
+    seeds: {
+      directory: "./db/seeds",
+    },
+  },
 };
