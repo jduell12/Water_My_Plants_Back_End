be934f575433d610c1f3f0166f9fdbb590782a9f
Jessica <duellje@oregonstate.edu>
2020-11-02 10:01:14 -0500

    add  auser to the db

--- users/usersModel.js
+++ users/usersModel.js
@@ -3,7 +3,9 @@ const db = require("../db/dbConfig");
 module.exports = { addUser, editUser, getUsers };
 
 //adds a user to the database
-function addUser() {}
+function addUser(user) {
+  return db("users").insert(user, "userid");
+}
 
 //edits user with the given id
 function editUser(userId) {}
--- users/usersModel.test.js
+++ users/usersModel.test.js
@@ -1,6 +1,79 @@
-const { it, expect, beforeEach } = require("@jest/globals");
+const { expect, it } = require("@jest/globals");
 const db = require("../db/dbConfig");
 const Users = require("./usersModel");
+
+//sample users to be used in tests
+const user1 = {
+  username: "wolf",
+  password: "pass",
+  email: "something@gmail.com",
+  firstname: "Wolf",
+  lastname: "Kelly",
+  phone: "7345553323",
+};
+const user2 = {
+  username: "sam",
+  password: "pass",
+  email: "",
+  firstname: "Sam",
+  lastname: "Gamgee",
+  phone: "7345523323",
+};
+const user3 = {
+  username: "dragon",
+  password: "pass",
+  email: "something@gmail.com",
+  firstname: "A",
+  lastname: "Dragon",
+  phone: "",
+};
+const user4 = {
+  username: "penguin",
+  password: "pass",
+  email: "something@gmail.com",
+  firstname: "",
+  lastname: "",
+  phone: "7345553333",
+};
+
+dbTestUsers = [
+  {
+    username: "wolf",
+    password: "pass",
+    email: "something@gmail.com",
+    firstname: "Wolf",
+    lastname: "Kelly",
+    phone: "7345553323",
+    userid: 1,
+  },
+  {
+    username: "sam",
+    password: "pass",
+    email: "",
+    firstname: "Sam",
+    lastname: "Gamgee",
+    phone: "7345523323",
+    userid: 2,
+  },
+  {
+    username: "dragon",
+    password: "pass",
+    email: "something@gmail.com",
+    firstname: "A",
+    lastname: "Dragon",
+    phone: "",
+    userid: 3,
+  },
+  {
+    username: "penguin",
+    password: "pass",
+    email: "something@gmail.com",
+    firstname: "",
+    lastname: "",
+    phone: "7345553333",
+    userid: 4,
+  },
+];
 
 describe("usersModel", () => {
   //wipes all tables in database clean so each test starts with empty tables
@@ -56,6 +129,31 @@ describe("usersModel", () => {
       const dbUsers = await db("users");
       expect(users).toHaveLength(4);
       expect(users).toEqual(dbUsers);
+    });
+  });
+
+  //adds users to the database
+  describe("addUsers", () => {
+    it("adds a user to an empty db", async () => {
+      await Users.addUser(user1);
+
+      const users = await db("users");
+      const shouldGet = [dbTestUsers[0]];
+
+      expect(users).toHaveLength(1);
+      expect(users).toEqual(shouldGet);
+    });
+
+    it("adds a user to a non-empty db", async () => {
+      await db("users").insert(user1);
+      await db("users").insert(user2);
+      await db("users").insert(user3);
+      await Users.addUser(user4);
+
+      const users = await db("users");
+
+      expect(users).toHaveLength(4);
+      expect(users).toEqual(dbTestUsers);
     });
   });
 });
