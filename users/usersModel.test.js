c869a9a5e1d382a39f69694f0aba448154498df1
Jessica <duellje@oregonstate.edu>
2020-11-02 09:37:34 -0500

    get users working

--- users/usersModel.test.js
+++ users/usersModel.test.js
@@ -13,9 +13,49 @@ describe("usersModel", () => {
 
   //gets all users from database
   describe("getUsers()", () => {
-    it.only("gets an empty array of users from empty db", async () => {
+    it("gets an empty array of users from empty db", async () => {
       const users = await Users.getUsers();
       expect(users).toHaveLength(0);
+    });
+
+    it("gets an array of users from a non-empty db", async () => {
+      await db("users").insert({
+        username: "wolf",
+        password: "pass",
+        email: "something@gmail.com",
+        firstname: "Wolf",
+        lastname: "Kelly",
+        phone: "7345553323",
+      });
+      await db("users").insert({
+        username: "sam",
+        password: "pass",
+        email: "",
+        firstname: "Sam",
+        lastname: "Gamgee",
+        phone: "7345523323",
+      });
+      await db("users").insert({
+        username: "dragon",
+        password: "pass",
+        email: "something@gmail.com",
+        firstname: "A",
+        lastname: "Dragon",
+        phone: "",
+      });
+      await db("users").insert({
+        username: "penguin",
+        password: "pass",
+        email: "something@gmail.com",
+        firstname: "",
+        lastname: "",
+        phone: "7345553333",
+      });
+
+      const users = await Users.getUsers();
+      const dbUsers = await db("users");
+      expect(users).toHaveLength(4);
+      expect(users).toEqual(dbUsers);
     });
   });
 });
