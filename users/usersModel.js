4e9320efd24d8a9be0c7fd2e5359cf19edbd5ba6
Jessica <duellje@oregonstate.edu>
2020-11-02 09:31:51 -0500

    get users working with empty db

--- users/usersModel.js
+++ users/usersModel.js
@@ -1,9 +1,14 @@
 const db = require("../db/dbConfig");
 
-module.exports = { addUser, editUser };
+module.exports = { addUser, editUser, getUsers };
 
 //adds a user to the database
 function addUser() {}
 
 //edits user with the given id
 function editUser(userId) {}
+
+//gets all users from the database
+function getUsers() {
+  return db("users");
+}
--- users/usersModel.test.js
+++ users/usersModel.test.js
@@ -1,13 +1,21 @@
+const { it, expect, beforeEach } = require("@jest/globals");
 const db = require("../db/dbConfig");
-const Users = require('./usersModel')
+const Users = require("./usersModel");
 
-describe("usersModel", ()=>{
-    //wipes all tables in database clean so each test starts with empty tables 
-    await db('users').truncate();
-    await db('plants').truncate();
+describe("usersModel", () => {
+  //wipes all tables in database clean so each test starts with empty tables
+  beforeEach(async () => {
+    //db is the knex initialized object using db.raw to truncate postgres tables with foreign keys
+    //can use knex.raw but it is global and deprecated
+    await db.raw("TRUNCATE TABLE plants RESTART IDENTITY CASCADE");
+    await db.raw("TRUNCATE TABLE users RESTART IDENTITY CASCADE");
+  });
 
-    //gets all users from database
-    describe('getUsers()', ()=>{
-        
-    })
-})
+  //gets all users from database
+  describe("getUsers()", () => {
+    it.only("gets an empty array of users from empty db", async () => {
+      const users = await Users.getUsers();
+      expect(users).toHaveLength(0);
+    });
+  });
+});
