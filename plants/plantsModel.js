3e08fcee484473f0464fb5b2b6497c4c489dec65
Jessica <duellje@oregonstate.edu>
2020-11-19 17:15:21 -0500

    tested addPlant function

--- plants/plantsModel.js
+++ plants/plantsModel.js
@@ -1,5 +1,24 @@
+const db = require("../db/dbConfig");
+
 module.exports = {
   addPlant,
   editPlant,
   deletePlant,
 };
+
+//adds a plant and returns the id of the new plant object in the database
+function addPlant(plant) {
+  return db("plants").insert(plant, "plantid");
+}
+
+//edits plant with given id and returns the count of records changed
+function editPlant(plantid, plantEdits) {
+  return db("plants")
+    .where({ plantid })
+    .update(plantEdits)
+    .then((count) => {
+      return count;
+    });
+}
+
+function deletePlant() {}
--- plants/plantsModel.test.js
+++ plants/plantsModel.test.js
@@ -1,30 +1,37 @@
 const db = require("../db/dbConfig");
-const plants = require('./plantsModel')
+const Plants = require("./plantsModel");
 
-//sample plants to be used in tests 
-function getTestPlants(){
-    return [
-        {name: 'Franky',
-        water_frequency: '1x/month',
-        species: '',
-        image: '',
-        }, 
-        {name: 'Eden',
-        water_frequency: '1x/day',
-        species: '',
-        image: '',
-        },
-        {name: 'WolfStar',
-        water_frequency: '1x/week',
-        species: '',
-        image: '',
-        },
-        {name: 'Sir McThirst',
-        water_frequency: '1x/hour',
-        species: '',
-        image: '',
-        }
-    ]
+//sample plants to be used in tests
+function getTestPlants() {
+  return [
+    {
+      name: "Franky",
+      water_frequency: "1x/month",
+      species: "",
+      image: "",
+      userid: 3,
+    },
+    {
+      name: "Eden",
+      water_frequency: "1x/day",
+      species: "",
+      image: "",
+      userid: 4,
+    },
+    {
+      name: "WolfStar",
+      water_frequency: "1x/week",
+      species: "",
+      image: "",
+      userid: 1,
+    },
+    {
+      name: "Sir McThirst",
+      water_frequency: "1x/hour",
+      species: "",
+      image: "",
+    },
+  ];
 }
 
 //sample users to be used in tests
@@ -75,4 +82,48 @@ describe("usersModel", () => {
     await db.raw("TRUNCATE TABLE plants RESTART IDENTITY CASCADE");
     await db.raw("TRUNCATE TABLE users RESTART IDENTITY CASCADE");
   });
-}
+
+  describe("addPlant(plant)", () => {
+    it("adds plant to empty database", async () => {
+      let users = getTestUsers();
+      let plants = getTestPlants();
+
+      for (let i = 0; i < users.length; i++) {
+        await db("users").insert(users[i]);
+      }
+
+      const dbUsers = await db("users");
+      expect(dbUsers.length).toBe(4);
+
+      let count = await Plants.addPlant(plants[1]);
+
+      const dbPlants = await db("plants");
+      expect(dbPlants.length).toBe(1);
+    });
+
+    it("adds plant non-empty database", async () => {
+      let users = getTestUsers();
+      let plants = getTestPlants();
+
+      for (let i = 0; i < users.length; i++) {
+        await db("users").insert(users[i]);
+      }
+
+      for (let i = 0; i < plants.length - 1; i++) {
+        await db("plants").insert(plants[i]);
+      }
+
+      const dbUsers = await db("users");
+      expect(dbUsers.length).toBe(4);
+
+      let dbPlants = await db("plants");
+      expect(dbPlants.length).toBe(3);
+
+      let count = await Plants.addPlant(plants[3]);
+
+      dbPlants = await db("plants");
+      expect(dbPlants.length).toBe(4);
+      expect(count).toEqual([4]);
+    });
+  });
+});
