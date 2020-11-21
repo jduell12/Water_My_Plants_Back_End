1f0514b25c59c95ee8370fe28111b077ee6da1df
Jessica <duellje@oregonstate.edu>
2020-11-21 16:31:22 -0500

    set up basic server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,10 @@
 const express = require("express");
+const cors = require("cors");
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.get("/", (req, res) => {
+  res.status(200).json({ server: "working!" });
+});
